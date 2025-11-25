6aeaa1e2818ebc882c915af725844ad105345272
NicolleValovicova <302245247@student.rocmondriaan.nl>
2025-11-25 22:47:53 +0100

    style formatted documents

--- js/boardLogic.js
+++ js/boardLogic.js
@@ -1,44 +1,100 @@
-let playerTurn = document.querySelector("#currentPlayerTurn")
+let playerTurn = document.querySelector("#currentPlayerTurn");
 const width = 8;
 let startingPlayer = "black";
-playerTurn.textContent = "black"
+playerTurn.textContent = "black";
 //! later add if select you r flock === black startingPlayer black
 const startPieces = [
-   blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook,
-   blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,
-   "","","","","","","","",
-   "","","","","","","","",
-   "","","","","","","","",
-   "","","","","","","","",
-   whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,
-   whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook
+  blackRook,
+  blackKnight,
+  blackBishop,
+  blackQueen,
+  blackKing,
+  blackBishop,
+  blackKnight,
+  blackRook,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whiteRook,
+  whiteKnight,
+  whiteBishop,
+  whiteQueen,
+  whiteKing,
+  whiteBishop,
+  whiteKnight,
+  whiteRook,
 ];
 let config = {
-  position: 'start',
+  position: "start",
   showNotation: true,
   draggable: true,
-  dropOffBoard: 'snapback',
+  dropOffBoard: "snapback",
   snapbackSpeed: 200,
-  snapSpeed: 50
-}
+  snapSpeed: 50,
+};
 
 // draw the board using js
 // draw pieces on board
 
 let boardDiv = document.querySelector(".board");
 function drawBoard() {
-  startPieces.forEach((startPiece, i) =>{
+  startPieces.forEach((startPiece, i) => {
     const square = document.createElement("div");
     square.classList.add("square");
     square.innerHTML = startPiece;
     square.firstChild && square.firstChild.setAttribute("draggable", true);
     square.setAttribute("square-id", i);
     // reverses the index so board aligns
-    const row = Math.floor((63 -i) / 8) +1;
+    const row = Math.floor((63 - i) / 8) + 1;
     if (row % 2 === 0) {
-      square.classList.add(i % 2 === 0 ? "black" : "white")
+      square.classList.add(i % 2 === 0 ? "black" : "white");
     } else {
-            square.classList.add(i % 2 === 0 ? "white" : "black")
+      square.classList.add(i % 2 === 0 ? "white" : "black");
     }
     boardDiv.appendChild(square);
   });
@@ -60,7 +116,7 @@ function drawLetters() {
     contain.innerHTML = letter;
     lettersDiv.appendChild(contain);
   }
-  
+
   for (let y = 8; y > 0; y--) {
     let containZ = document.createElement("p");
     const number = y;
@@ -74,100 +130,99 @@ const allSquares = document.querySelectorAll(".square");
 const allPieces = document.querySelectorAll(".piece");
 console.log(allSquares);
 
-allPieces.forEach(piece =>{
-
+allPieces.forEach((piece) => {
   piece.addEventListener("dragstart", dragStart);
-})
+});
 
-allSquares.forEach(square =>{
+allSquares.forEach((square) => {
   square.addEventListener("dragover", dragOver);
   square.addEventListener("drop", dragDrop);
 });
 
 let startPosId;
 let draggedElement;
-function dragStart(e){
+function dragStart(e) {
   draggedElement = e.currentTarget;
- startPosId = draggedElement.parentElement.getAttribute("square-id");
+  startPosId = draggedElement.parentElement.getAttribute("square-id");
 }
 
-function dragOver(e){
+function dragOver(e) {
   e.preventDefault();
 }
 
-function dragDrop(e){
-e.preventDefault();
-console.log(e.target);
-const correctGo= draggedElement.firstChild.classList.contains(startingPlayer)
-const taken = e.target.classList.contains("piece");
-const valid = checkIfValid(e.target);
-const opponentGo = startingPlayer === "white" ? "black " : "white";
-const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
-const dropSquare = e.target.closest(".square");
+function dragDrop(e) {
+  e.preventDefault();
+  console.log(e.target);
+  const correctGo =
+    draggedElement.firstChild.classList.contains(startingPlayer);
+  const taken = e.target.classList.contains("piece");
+  const valid = checkIfValid(e.target);
+  const opponentGo = startingPlayer === "white" ? "black " : "white";
+  const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo);
+  const dropSquare = e.target.closest(".square");
 
-if(correctGo){
-  // must check first this move
-  if (takenByOpponent && valid) {
-    if(!dropSquare) return;
-    const existingPiece = dropSquare.querySelector(".piece");
-    if (existingPiece) existingPiece.remove();
-    changePlayer();
-    return
+  if (correctGo) {
+    // must check first this move
+    if (takenByOpponent && valid) {
+      if (!dropSquare) return;
+      const existingPiece = dropSquare.querySelector(".piece");
+      if (existingPiece) existingPiece.remove();
+      changePlayer();
+      return;
+    }
+    // then check this move
+    if (taken && !takenByOpponent) {
+      return;
+    }
+    if (valid) {
+      e.target.append(draggedElement);
+      changePlayer();
+      return;
+    }
   }
-  // then check this move
-  if (taken && !takenByOpponent) {
-    return
-  }
-  if(valid){
-    e.target.append(draggedElement)
-    changePlayer();
-    return
-  }
+
+  dropSquare.appendChild(draggedElement);
 }
 
-dropSquare.appendChild(draggedElement)
-}
-
-function checkIfValid(target){
-  const targetId = Number(target.getAttribute("square-id")) || Number(target.parentNode.getAttribute("square-id"));
+function checkIfValid(target) {
+  const targetId =
+    Number(target.getAttribute("square-id")) ||
+    Number(target.parentNode.getAttribute("square-id"));
   const startId = Number(startPosId);
-  const piece =draggedElement;
+  const piece = draggedElement;
   console.log("start id is:" + startId);
   console.log("target id is:" + targetId);
   console.log("piece id is:", piece.id);
-  
+
   switch (piece) {
     case "blackPawn":
-      
       break;
-  
+
     default:
       break;
   }
 }
 
-function changePlayer(){
-  if (startingPlayer === "black"){
+function changePlayer() {
+  if (startingPlayer === "black") {
     reverseIds();
-    startingPlayer = "white"
-playerTurn.textContent = "white"  
-} else{
-  revertIds();
-  startingPlayer = "black"
-  playerTurn.textContent = "black"  
-
+    startingPlayer = "white";
+    playerTurn.textContent = "white";
+  } else {
+    revertIds();
+    startingPlayer = "black";
+    playerTurn.textContent = "black";
+  }
 }
-}
 
-function reverseIds(){
+function reverseIds() {
   const allSquares = document.querySelectorAll(".square");
-  allSquares.forEach((square, i) =>{
-    square.setAttribute("square-id", (width * width -1) -i)
-  }) 
+  allSquares.forEach((square, i) => {
+    square.setAttribute("square-id", width * width - 1 - i);
+  });
 }
 
-function revertIds(){
-    const allSquares = document.querySelectorAll(".square");
-  allSquares.forEach((square, i) =>square.setAttribute("square-id", i))
-
+function revertIds() {
+  const allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square, i) => square.setAttribute("square-id", i));
 }
--- js/script-nicolle.js
+++ js/script-nicolle.js
@@ -1,42 +1,41 @@
-
 const startScreen = document.querySelector("#start-screen");
 const menuScreen = document.querySelector("#menu-screen");
 const gameIndex = document.querySelector("#game-index");
 let menuPlayBtn = document.querySelector("#menuPlayBtn");
 let exitBtn = document.querySelector(".exit");
-function hidePages(){
-    startScreen.classList.add("notVisible");
-    menuScreen.classList.add("notVisible");
-    gameIndex.classList.add("notVisible");
+function hidePages() {
+  startScreen.classList.add("notVisible");
+  menuScreen.classList.add("notVisible");
+  gameIndex.classList.add("notVisible");
 }
 hidePages();
 // first call the hide all pages then display the start page and after the game load bar has loaded (some sort of interval) call the settimeout and hide all pages again and after that show the menu screen
 gameIndex.classList.remove("notVisible");
 menuPlayBtn.addEventListener("click", () => {
-    hidePages();
-    gameIndex.classList.remove("notVisible");
-})
+  hidePages();
+  gameIndex.classList.remove("notVisible");
+});
 exitBtn.addEventListener("click", () => {
-    hidePages();
-    startScreen.classList.remove("notVisible");
-})
+  hidePages();
+  startScreen.classList.remove("notVisible");
+});
 
-// script for start page 
+// script for start page
 let crowfactOutput = document.querySelector("#crowfactOutput");
 let crowFacts = [
-    "A group of crows is called a 'murder'.",
-"Crows can solve multi-step puzzles, making them one of the smartest animals on Earth.",
-"Crows hold funerals: when one crow dies, the whole murder gathers to inspect the body and learn from the danger.",
-"Jackdaws form lifelong partnerships, just like chess pieces protecting each other on the board.",
-"When crows work in pairs, one distracts while the other steals — teamwork that mirrors chess strategy.",
-"Ravens play mind games, like teasing wolves or sliding down snowy roofs for fun—just like psychological warfare on a chessboard.",
-"Crows hold grudges: if one bird wrongs them, they remember it for generations—and the entire murder may retaliate."
-]
-let i = 0; 
-  crowfactOutput.textContent = crowFacts[i]; 
-setInterval(() => { 
-  i = (i + 1) % crowFacts.length; 
-  crowfactOutput.textContent = crowFacts[i]; 
+  "A group of crows is called a 'murder'.",
+  "Crows can solve multi-step puzzles, making them one of the smartest animals on Earth.",
+  "Crows hold funerals: when one crow dies, the whole murder gathers to inspect the body and learn from the danger.",
+  "Jackdaws form lifelong partnerships, just like chess pieces protecting each other on the board.",
+  "When crows work in pairs, one distracts while the other steals — teamwork that mirrors chess strategy.",
+  "Ravens play mind games, like teasing wolves or sliding down snowy roofs for fun—just like psychological warfare on a chessboard.",
+  "Crows hold grudges: if one bird wrongs them, they remember it for generations—and the entire murder may retaliate.",
+];
+let i = 0;
+crowfactOutput.textContent = crowFacts[i];
+setInterval(() => {
+  i = (i + 1) % crowFacts.length;
+  crowfactOutput.textContent = crowFacts[i];
 }, 5000);
 
 // script for menu page
@@ -51,22 +50,22 @@ let falseSettingOptions = document.querySelectorAll(".settingOpt2");
 
 attunement.classList.add("notVisible");
 legacyContain.classList.add("notVisible");
-attunementMenu.addEventListener("click", () =>{
-    attunement.classList.toggle("notVisible");
-    legacyContain.classList.add("notVisible");
-})
+attunementMenu.addEventListener("click", () => {
+  attunement.classList.toggle("notVisible");
+  legacyContain.classList.add("notVisible");
+});
 
-atuItems.forEach((item, index) =>{
-    item.addEventListener("click", () => {
- settingIndexes.forEach(i => i.classList.remove("visible"));
-    settingIndexes[index].classList.add("visible")
-    })
+atuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    settingIndexes.forEach((i) => i.classList.remove("visible"));
+    settingIndexes[index].classList.add("visible");
+  });
 });
 settingIndexes[0].classList.add("visible");
 
-creditsMenu.addEventListener("click", () =>{
-    attunement.classList.add("notVisible");
-    legacyContain.classList.toggle("notVisible");
+creditsMenu.addEventListener("click", () => {
+  attunement.classList.add("notVisible");
+  legacyContain.classList.toggle("notVisible");
 });
 
 // script for side bar in game
@@ -77,10 +76,10 @@ let gameSidebar = document.querySelector(".gameSidebar");
 
 arrowBack.addEventListener("click", closeNav);
 arrowForward.addEventListener("click", openNav);
-quitbtn.addEventListener("click", () =>{
-    hidePages();
-    menuScreen.classList.remove("notVisible");
-})
+quitbtn.addEventListener("click", () => {
+  hidePages();
+  menuScreen.classList.remove("notVisible");
+});
 function openNav() {
   gameSidebar.style.width = "300px";
   arrowBack.style.display = "block";
